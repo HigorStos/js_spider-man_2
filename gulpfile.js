81de9557c124a6bf62c1f2d32a4b94629ab84d7c
Higor Santos <higor.stos@outlook.com>
2023-08-19 18:06:12 -0300

    feat: javascript minified

--- gulpfile.js
+++ gulpfile.js
@@ -15,13 +15,13 @@ function images() {
         .pipe(gulp.dest('./dist/images'))
 }
 
-function js() {
+function scripts() {
     return gulp.src('./src/scripts/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./dist/js'))
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
